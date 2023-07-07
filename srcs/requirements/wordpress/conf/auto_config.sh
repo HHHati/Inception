5b38d5b78a05b258a9858402fbc54d85586a11bf
Basile De Leeuw <basile_deleeuw@yahoo.be>
2023-07-07 12:16:01 +0200

    Ven  7 jul 2023 12:16:01 CEST -> 2.0

---
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -0,0 +1,7 @@
+sleep 10
+
+wp config create	--allow-root \
+					--dbname=$MYSQL_DATABASE \
+					--dbuser=$MYSQL_USER \
+					--dbpass=$MYSQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
