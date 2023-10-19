59474d1c9907add0b5677ef5dfd007c0d0178cab
Basile De leeuw <bade-lee@fu-r3-p1.s19.be>
2023-10-19 16:02:57 +0200

    2.4

--- srcs/requirements/mariadb/conf/config.sh
+++ srcs/requirements/mariadb/conf/config.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
-	service mysql start --datadir=/var/lib/mysql
+    service mysql start --datadir=/var/lib/mysql
 
-	echo "creation of database : $MARIADB_DATABASE"
-	eval "echo \"$(cat config.sql)\"" | mariadb -u root
-	mysqladmin -u root password $MARIADB_ROOT_PASSWORD
+    echo "creation of database : $MARIADB_DATABASE"
+    eval "echo \"$(cat config.sql)\"" | mariadb -u root
+    mysqladmin -u root password $MARIADB_ROOT_PASSWORD
 
-	service mysql stop --datadir=/var/lib/mysql
+    service mysql stop --datadir=/var/lib/mysql
 fi
 
 echo "$MARAIDB_DATABASE is ready to be used"
