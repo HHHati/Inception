0cf3603cd23518a7a21a5de99acbb6011a948628
Basile De leeuw <bade-lee@fu-r2-p4.s19.be>
2023-10-21 16:51:28 +0200

    4.8

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Transfer ownership of database dir to mysql user. 
-# This is a common step in setting up a MySQL server because MySQL 
-# typically runs as the mysql user, and it requires appropriate permissions to access and modify its data directory.
 chown -R mysql:mysql /var/lib/mysql
 
-#create the sql file with the commands to change root pass, create db and user and give privileges to db to user
 cat << EOF > createdb.sql
 USE mysql;
 FLUSH PRIVILEGES;
@@ -19,11 +15,8 @@ GRANT ALL PRIVILEGES ON ${MYSQL_NAME}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-#bootstrap server and initialize data with the sql file created
 mysqld --bootstrap --datadir=/var/lib/mysql --user=mysql < createdb.sql
 
-#remove the file
 rm createdb.sql
 
-#starting and stopping mariadb automatically
 mysqld_safe
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# create directory to use in nginx container later and also to setup the wordpress conf
 mkdir /var/www/
 mkdir /var/www/html
 
@@ -8,7 +7,6 @@ cd /var/www/html
 
 rm -rf *
 
-#execute only if wordpress is not installed
 if [ ! -f /var/www/html/wp-config.php ]; then
 
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
@@ -19,7 +17,6 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
 	wp core download --allow-root
 
-	#setup wp-config.php file with the proper values
 	sed -i -r "s/database_name_here/$MYSQL_NAME/1"   /var/www/html/wp-config-sample.php
 	sed -i -r "s/username_here/$MYSQL_USER/1"  /var/www/html/wp-config-sample.php
 	sed -i -r "s/password_here/$MYSQL_PASSWORD/1"    /var/www/html/wp-config-sample.php
@@ -27,7 +24,6 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
 	cp wp-config-sample.php wp-config.php
 
-	#install and configure wordpress and create a new user
 	wp core install --url=${DOMAIN_NAME} \
 					--title=${WP_TITLE} \
 					--admin_user=$WP_ADMIN_USER \
@@ -38,14 +34,11 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
 	wp user create  $WP_USER \
 					$WP_USER_EMAIL \
-					--role=$WP_ROLE \
 					--user_pass=$WP_USER_PASSWORD \
 					--allow-root
 
-	#Install a theme
 	wp theme install inspiro --activate --allow-root
 
 fi
 
-#start the PHP-FPM (FastCGI Process Manager) service for PHP version 7.3 in foreground mode
 /usr/sbin/php-fpm7.4 -F
